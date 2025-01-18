4fab1c61dcf7b68bce330d7f8624dc475d65d0c2
Matheus-Henrique-Santos <matheusexeop@hotmail.com>
2025-01-17 23:22:13 -0300

    initial permissions

---
+++ app/Livewire/RolesManagement.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class RolesManagement extends Component
+{
+    public function render()
+    {
+        return view('livewire.roles-management');
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -64,4 +64,27 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function permissions()
+    {
+        return $this->belongsToMany(Permission::class);
+    }
+
+    public function roles()
+    {
+        return $this->belongsToMany(Role::class);
+    }
+
+    public function hasRole($role)
+    {
+        if (is_string($role)) {
+            return $this->roles->contains('name', $role);
+        }
+        return !! $role->intersect($this->roles)->count();
+    }
+
+    public function hasPermission($permission)
+    {
+        return $this->roles->map->permissions->flatten()->contains('name', $permission);
+    }
 }
