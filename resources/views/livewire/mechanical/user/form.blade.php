86b8b0c379b3741b6fe87a5d28388c48f7292bf7
edsuuu <edsonteles343@gmail.com>
2025-02-18 09:30:02 -0300

    fix modal

--- resources/views/livewire/mechanical/user/form.blade.php
+++ resources/views/livewire/mechanical/user/form.blade.php
@@ -13,11 +13,11 @@
                 <x-input label="Nome" title="Utilize somente letras" wire:model.defer="state.name" autocomplete="off" error="name"/>
             </div>
             <div class="md:col-span-4">
-                <x-select label="Nível de acesso" wire:model.defer="state.role_id" error="role_id">
-                    @foreach($response->roles as $itemRole)
-                        <option value="{{$itemRole['id']}}">{{$itemRole['name']}}</option>
-                    @endforeach
-                </x-select>
+{{--                <x-select label="Nível de acesso" wire:model.defer="state.role_id" error="role_id">--}}
+{{--                    @foreach($response->roles as $itemRole)--}}
+{{--                        <option value="{{$itemRole['id']}}">{{$itemRole['name']}}</option>--}}
+{{--                    @endforeach--}}
+{{--                </x-select>--}}
             </div>
             <div class="md:col-span-4">
                 <x-input label="Telefone" wire:model.defer="state.phone" input-mask="phone" error="phone" autocomplete="off" />
@@ -32,16 +32,16 @@
                 <x-input type="password" wire:model.defer="state.confirm_password" autocomplete="off" error="confirm_password" label="Confirmar senha"/>
             </div>
             <div class="md:col-span-4">
-                <x-select wire:model.defer="state.status" error="status" label="Status">
-                    <option value="Ativo">Ativo</option>
-                    <option value="Inativo">Inativo</option>
-                </x-select>
+{{--                <x-select wire:model.defer="state.status" error="status" label="Status">--}}
+{{--                    <option value="Ativo">Ativo</option>--}}
+{{--                    <option value="Inativo">Inativo</option>--}}
+{{--                </x-select>--}}
             </div>
-            @if(Auth::user()->tenant->scope == 'Comercial')
-                <div class="md:col-span-4">
-                    <x-input type="text" wire:model.defer="state.reev_token" autocomplete="off" error="reev_token" label="Reev token"/>
-                </div>
-            @endif
+{{--            @if(Auth::user()->tenant->scope == 'Comercial')--}}
+{{--                <div class="md:col-span-4">--}}
+{{--                    <x-input type="text" wire:model.defer="state.reev_token" autocomplete="off" error="reev_token" label="Reev token"/>--}}
+{{--                </div>--}}
+{{--            @endif--}}
             <div class="md:col-span-12">
                 <x-button size="md">{{$response->user ? 'Salvar' : 'Cadastrar'}}</x-button>
             </div>
